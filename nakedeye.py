014ce9078c321ca10392f1a00e516f2c883d55a1
Ryan Keisler <rkeisler@gmail.com>
2014-01-06 13:16:00 -0800

    cleaned up

--- nakedeye.py
+++ nakedeye.py
@@ -3,59 +3,50 @@ import ipdb
 import matplotlib.pylab as plt
 from os import system
 plt.ioff()
-#plt.ion()
+
+# some global distances and RA/Dec's.
 klyr_to_center = 27.2
-ra_gal_center = 15.*(17. + 45.6/60.)
-dec_gal_center = -28.94
-ra_gal_npole = 15.*(12. + 51.4/60.)
-dec_gal_npole = 27.13
-ra_ecl_npole = 15.*18.0
-dec_ecl_npole = 66. + 33./60. + 38.55/3600.
 lyr_per_pc = 3.2616
+ra_gal_center = 266.4
+dec_gal_center = -28.94
+ra_gal_npole = 192.85
+dec_gal_npole = 27.13
+ra_ecl_npole = 270.0
+dec_ecl_npole = 66.561
 
-
+# some global font stuff
 fontsize=20
 yspacing = fontsize/20*0.15
-textcolor = '#99FFFF'
 textcolor = 'white'
 charwidth=29
-# http://cdsarc.u-strasbg.fr/viz-bin/Cat?cat=I%2F239&target=http&
-# http://cdsarc.u-strasbg.fr/viz-bin/nph-Cat/fits?I%2F239/hip_main.dat.gz
 
 def main():
     d_right, d_up, mag = get_star_data()
     fig, ax = initialize_figure()
     draw_galaxy(ax)
-
     plot_stars(ax, d_right, d_up, mag, mag_min=-99, mag_max=3.0, color='#FFFFFC', psize=16, alpha=0.6)
     plot_stars(ax, d_right, d_up, mag, mag_min=3.0, mag_max=6.0, color='#FFCC66', psize=5, alpha=0.3)
-    #plot_stars(ax, d_right, d_up, mag, mag_min=3.0, mag_max=4.5, color='#FFFF66', psize=10, alpha=0.4)
-    #plot_stars(ax, d_right, d_up, mag, mag_min=4.5, mag_max=6.0, color='#FFCC66', psize=5, alpha=0.3)
-
     add_local_info(ax)
     convert_to_html_and_open(fig)
+
 
 def draw_galaxy(ax):
     from matplotlib.patches import Ellipse
 
-    # main disk
+    # MW main disk
     alpha=0.04
     nell = 10
     for w,h in zip(np.linspace(60,80,nell), np.linspace(1.0,2.0,nell)):
         ax.add_patch(Ellipse(xy=(klyr_to_center,0), width=w, height=h, angle=0, color='white', alpha=alpha))
 
-    # bulge
+    # MW bulge
     alpha=0.06
     nell = 5
     for w,h in zip(1.3*np.linspace(4,8,nell), np.linspace(4,8,nell)):
         ax.add_patch(Ellipse(xy=(klyr_to_center,0), width=w, height=h, angle=0, color='white', alpha=alpha))
     text_rectangle(klyr_to_center+0.1, 0.3, charwidth, fontsize, textcolor, yspacing, 'The center of the Milky Way is about 30,000 light-years away, and the light we see from it today was emitted around the time that humans began farming.')
-#   during the Upper Paleolithic, when humans were not yet farming but beginning to produce art')
-#   when early humans were painting in caves but not yet farming.')
 
-
-
-    # lmc
+    # LMC
     ra_lmc = 80.893
     dec_lmc = -69.75611
     dot_gal_center_lmc = dot_ra_dec(ra_gal_center, dec_gal_center, ra_lmc, dec_lmc)
@@ -73,7 +64,7 @@ def draw_galaxy(ax):
     text_rectangle(d_right_lmc+0.1, d_up_lmc+0.65, charwidth, fontsize, textcolor, yspacing, "Note that many of the objects shown on this page are even farther away than they appear to be.  You just can't see the distances into and out of the screen.")
 
 
-    # smc
+    # SMC
     ra_smc = 13.18666
     dec_smc = -72.828
     dot_gal_center_smc = dot_ra_dec(ra_gal_center, dec_gal_center, ra_smc, dec_smc)
@@ -90,7 +81,7 @@ def draw_galaxy(ax):
     text_rectangle(d_right_smc+0.1, d_up_smc+0.4, charwidth, fontsize, textcolor, yspacing, "The Small Magellanic Cloud is about 200,000 light-years from Earth.")
     text_rectangle(d_right_smc+0.1, d_up_smc-0.2, charwidth, fontsize, textcolor, yspacing, "The light we see from it originated right around the time that homo sapiens first appeared in Africa.")
 
-    # m31
+    # M31
     ra_m31 = 10.6845
     dec_m31 = 41.2691
     dot_gal_center_m31 = dot_ra_dec(ra_gal_center, dec_gal_center, ra_m31, dec_m31)
@@ -99,13 +90,12 @@ def draw_galaxy(ax):
     d_klyr_m31 = 2.54*1000.
     d_right_m31 = d_klyr_m31*dot_gal_center_m31
     d_up_m31 = d_klyr_m31*dot_gal_npole_m31
-    # main disk
+    # M31 main disk
     alpha=0.04
     nell = 10
     for w,h in zip(np.linspace(60,80,nell), 4.*np.linspace(1.1,2.4,nell)):
         ax.add_patch(Ellipse(xy=(d_right_m31,d_up_m31), width=w, height=h, angle=-30, color='white', alpha=alpha))
-
-    # bulge
+    # M31 bulge
     alpha=0.05
     nell = 5
     for w,h in zip(1.4*1.3*np.linspace(4,8,nell), 1.4*np.linspace(4,8,nell)):
@@ -118,11 +108,8 @@ def draw_galaxy(ax):
 
     text_rectangle(d_right_m31+x0, y0-1.6, charwidth, fontsize, textcolor, yspacing, "It's incredible that, thanks to some fluke of physics and evolution, we can see this far with our eyeballs!")
 
-    #x=d_right_m31+x0; y=d_up_m31-0.1
     x=d_right_m31+0.25; y=y0-1.6#d_up_m31-1.1
     text_rectangle(x, y, charwidth, fontsize, textcolor, yspacing, "Modern telescopes allow us to see much, much farther, but this is the end of the line for objects visible to the naked eye, and for this page.  I promise that, no matter how much you zoom out, you will never never get to the Cosmic Microwave Background.")
-
-
 
     # draw connecting lines
     linestyle='--'
@@ -131,15 +118,9 @@ def draw_galaxy(ax):
     d_right_mc = 0.5*(d_right_lmc+d_right_smc)
     d_up_mc = 0.5*(d_up_lmc+d_up_smc)
     plt.plot([0,klyr_to_center],[0,0], linestyle, color=color,linewidth=lw)
-    if False:
-        plt.plot([klyr_to_center,d_right_mc],[0,d_up_mc], linestyle, color=color,linewidth=lw)
-        plt.plot([d_right_mc, d_right_m31],[d_up_mc, d_up_m31], linestyle, color=color,linewidth=lw)
-    else:
-        plt.plot([klyr_to_center,d_right_lmc],[0,d_up_lmc], linestyle, color=color,linewidth=lw)
-        plt.plot([d_right_lmc, d_right_smc],[d_up_lmc, d_up_smc], linestyle, color=color,linewidth=lw)
-        plt.plot([d_right_smc, d_right_m31],[d_up_smc, d_up_m31], linestyle, color=color,linewidth=lw)
-
-
+    plt.plot([klyr_to_center,d_right_lmc],[0,d_up_lmc], linestyle, color=color,linewidth=lw)
+    plt.plot([d_right_lmc, d_right_smc],[d_up_lmc, d_up_smc], linestyle, color=color,linewidth=lw)
+    plt.plot([d_right_smc, d_right_m31],[d_up_smc, d_up_m31], linestyle, color=color,linewidth=lw)
     
 
 def add_local_info(ax):
@@ -150,20 +131,16 @@ def add_local_info(ax):
     plt.text(0,0.21, '200 lyr', color=textcolor, size=fontsize, horizontalalignment='center')
     plt.text(0,0.51, '500 lyr', color=textcolor, size=fontsize, horizontalalignment='center')
     plt.text(0,0.51, '500 lyr', color=textcolor, size=fontsize, horizontalalignment='center')
-    
     coltmp = '#666666'
     plt.text(0, -0.68-0.045, 'star positions from', color=coltmp, size=fontsize, horizontalalignment='center')
     plt.text(0, -0.81-0.045, 'the Hipparcos sattelite', color=coltmp, size=fontsize, horizontalalignment='center')
-
     x0=-2.6; y0=0.4
     text_rectangle(x0, y0, charwidth, fontsize, textcolor, yspacing, 'The stars you can see with your naked eye are typically several hundreds of light-years away.')
     text_rectangle(x0, y0-0.55, charwidth, fontsize, '#FFFFFC', yspacing, 'The white stars show what you might see from a city,')
     text_rectangle(x0, y0-0.85, charwidth, fontsize, '#FFFFFC', yspacing, 'the yellow stars from the country.')
-
-
-
     text_rectangle(x0, -1, charwidth, fontsize, '#666666', yspacing, 'Zoom in and out to explore.')
-    text_rectangle(1.0, 0.03, 4*charwidth, fontsize, '#995555', yspacing, '>  to the center of the Milky Way  >')
+    text_rectangle(1.0, 0.06, 4*charwidth, fontsize, '#995555', yspacing, '>  to the center of the Milky Way  >')
+    text_rectangle(1.0, -0.14, 4*charwidth, fontsize, '#995555', yspacing, '>  check it out  >')
 
 
 def text_rectangle(x, y, charwidth, fontsize, color, yspacing, thetext):
@@ -179,10 +156,7 @@ def text_rectangle(x, y, charwidth, fontsize, color, yspacing, thetext):
             nlines+=1
             plt.text(x, y-nlines*yspacing, line, color=color, size=fontsize)
             line=''
-    
-    
-    
-    
+
 
 def convert_to_html_and_open(fig):
     from mpld3 import fig_to_d3
@@ -197,20 +171,14 @@ def convert_to_html_and_open(fig):
 def plot_stars(ax, d_right, d_up, mag, mag_min=0, mag_max=3.0, 
                color='#FFFF99', psize=6, alpha=0.3):
     wh=np.where((mag>mag_min)&(mag<=mag_max))[0]
-    #wh=wh[0:-1:1]
     plt.scatter(d_right[wh], d_up[wh], 
                 alpha=alpha, s=psize, 
                 color=color, linewidths=0)
 
 
 def initialize_figure():
-    aratio = 2.3
-    sf = 5.2
-
     aratio = 1.9
     sf = 6.5
-
-    #minx=-6;maxx=33.5
     minx=-2.9;maxx=2.9
     miny=-0.5*(maxx-minx)/aratio;
     maxy=0.5*(maxx-minx)/aratio;
@@ -224,24 +192,6 @@ def initialize_figure():
     return fig, ax
 
     
-def get_star_data_old():
-    from pandas.io.parsers import read_csv
-    d=read_csv('../HYG-Database/hygxyz.csv')
-    ra=15.*d['RA']; dec=d['Dec']; mag=d['Mag'];
-    dis=d['Distance']*lyr_per_pc/1000. #convert from parsec to klyr
-    dot_gal_center = dot_ra_dec(ra_gal_center, dec_gal_center, ra, dec)
-    dot_gal_npole = dot_ra_dec(ra_gal_npole, dec_gal_npole, ra, dec)
-    dist_gal_center = dis*dot_gal_center
-    dist_gal_npole = dis*dot_gal_npole
-    
-    #tempp!#tmpp!  convert to equatorial
-    wh=np.where(np.abs(dec)>30.)[0]
-    dist_gal_center = dist_gal_center[wh]
-    dist_gal_npole = dist_gal_npole[wh]
-    mag = mag[wh]
-
-    return dist_gal_center, dist_gal_npole, mag
-
 def dot_ra_dec(ra0, dec0, ra, dec):
     # ra/dec should be in degrees.
     d2r = np.pi/180.
@@ -264,6 +214,7 @@ def get_star_data(quick=True):
     if quick: ra, dec, plx, e_plx, Vmag, d_kpc, d_klyr = pickle.load(open('get_star_data.pkl','r'))
     else:
         from astropy.io import fits
+        # from http://cdsarc.u-strasbg.fr/viz-bin/Cat?cat=I%2F239&target=http&
         d=fits.open('I_239_hip_main.dat.gz.fits')[1].data
         frac_err = 0.2
         max_Vmag = 6.0
@@ -287,12 +238,4 @@ def get_star_data(quick=True):
     d_gal_center = d_klyr*dot_gal_center
     d_gal_npole = d_klyr*dot_gal_npole
     return d_gal_center, d_gal_npole, Vmag
-    
-    
-    
-
-    
-
-
-
     
